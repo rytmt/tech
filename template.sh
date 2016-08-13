56fc5e732fd468d9dc1e74c3f74b5715d1bea59c
rytmt <ryoto0114@gmail.com>
2016-08-13 17:05:53 +0900

    misc

---
+++ template.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# clear all alias if defined unalias command
+[ "$(type unalias >/dev/null 2>&1)" ] && unalias -a
+
+# error occur when refer undefined variable
+set -u
+
+# set default permission
+umask 0022
+
+# env variables
+PATH='/bin:/usr/bin:/sbin:/usr/sbin'
+IFS=$(printf ' \t\n_'); IFS=${IFS%_}
+LANG=C; LC_ALL=C
+export PATH IFS LANG LC_ALL
